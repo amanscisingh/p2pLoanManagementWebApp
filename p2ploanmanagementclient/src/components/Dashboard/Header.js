f77692a232080fb926ea4add0da1c72a24c194d8
Aman Singh <aman.singh.civ19@itbhu.ac.in>
2022-05-11 08:35:57 +0530

    some other changes

--- p2ploanmanagementclient/src/components/Dashboard/Header.js
+++ p2ploanmanagementclient/src/components/Dashboard/Header.js
@@ -6,9 +6,9 @@ const Header = () => {
 
   return (
     <div className='headerContainer'>
-        <img src={userInfo.photo} alt="logo" srcset="" width="80px" height="80px" style={{ borderRadius:"50%" }} />
+        <img src={userInfo.photo} alt="logo" srcset="" width="50px" height="50px" style={{ borderRadius:"50%" }} />
         <h1>{userInfo.name}</h1>
-        <img src={userInfo.photo} alt="profile photo" srcset="" width="80px" height="80px" style={{ borderRadius:"50%" }} />
+        <img src={userInfo.photo} alt="profile photo" srcset="" width="50px" height="50px" style={{ borderRadius:"50%" }} />
     </div>
   )
 }
--- p2ploanmanagementclient/src/components/Dashboard/Profile.js
+++ p2ploanmanagementclient/src/components/Dashboard/Profile.js
@@ -2,7 +2,75 @@ import React from 'react'
 
 const Profile = () => {
   return (
-    <div className='bottomContainer'>Profile</div>
+    <div className='bottomContainer'>
+      <div className="generalInformation">
+        <label htmlFor="profilePic">Change profile pic</label>
+        <input type="file" name="profilePic" id="profilePic" />
+        <button>Upload</button>
+        <br />
+
+        <label htmlFor="Name"> Name </label>
+        <input type="text" name="name" id="name" />
+        <br />
+
+        <label htmlFor="phNo">Phone No.</label>
+        <input type="text" name="phNo" id="phNo" />
+        <br />
+
+        <label htmlFor="description">Description</label>
+        <textarea name="description" id="description" cols="30" rows="10"></textarea>
+        <br />
+
+      </div>
+
+      <div className="questionsSection">
+        <h3>Bank Details</h3>
+        <label htmlFor="bankName">Bank Name</label>
+        <input type="text" name="bankName" id="bankName" />
+        <br />
+
+        <label htmlFor="bankAccountNo">Bank Account No.</label>
+        <input type="text" name="bankAccountNo" id="bankAccountNo" />
+        <br />
+
+        <label htmlFor="bankIfsc">Bank IFSC Code</label>
+        <input type="text" name="bankIfsc" id="bankIfsc" />
+        <br />
+
+        <h3>Pan Details</h3>
+        <label htmlFor="panNo">Pan No.</label>
+        <input type="text" name="panNo" id="panNo" />
+        <br />
+
+        <label htmlFor="panDocument">PAN Document</label>
+        <input type="file" name="panDocument" id="panDocument" />
+        <button>Upload</button>
+        <br />
+
+        <h3>Adhaar</h3>
+        <label htmlFor="adhaarNo">Adhaar No.</label>
+        <input type="text" name="adhaarNo" id="adhaarNo" />
+        <br />
+
+        <label htmlFor="adhaarDocument">Adhaar Document</label>
+        <input type="file" name="adhaarDocument" id="adhaarDocument" />
+        <button>Upload</button>
+        <br />
+
+        <h3>Salery Slip (Upload One By One | Uploaded Ones will appear below) </h3>
+        <label htmlFor="salerySlip">Salery Slip</label>
+        <input type="file" name="salerySlip" id="salerySlip" />
+        <input type="text" name="saleryAmount" id="saleryAmount" placeholder='Enter Amount (in Rs)' />
+        <button>Upload</button>
+        <br />
+
+        <h3>CTC Information</h3>
+        <label htmlFor="ctc">CTC</label>
+        <input type="text" name="ctc" id="ctc" />
+        <br />
+
+      </div>
+    </div>
   )
 }
 
